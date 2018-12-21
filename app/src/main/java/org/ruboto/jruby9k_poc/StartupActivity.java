08caf1c3c8865b378ddf54e0eb316480d981f6df
Uwe Kubosch <uwe@datek.no>
2018-12-21 14:04:42 +0100

    first commit

---
+++ app/src/main/java/org/ruboto/jruby9k_poc/StartupActivity.java
@@ -0,0 +1,13 @@
+package org.ruboto.jruby9k_poc;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StartupActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_startup);
+    }
+}
