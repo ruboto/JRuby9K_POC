eb3ffab5b1c97f99b574c551fda536b7105551a8
Uwe Kubosch <uwe@datek.no>
2019-08-13 09:55:20 +0200

    Update script for updating JRuby

--- app/update_jruby_jar.sh
+++ app/update_jruby_jar.sh
@@ -1,30 +1,38 @@
 #!/usr/bin/env bash -e
 
-BRANCH="9.2.8.0"
-VERSION="${BRANCH}-20190729.233104-80"
-JAR_FILE="jruby-complete-${VERSION}.jar"
-M2_CACHED_JAR="~/.m2/repository/org/jruby/jruby-complete/${VERSION}/jruby-complete-${VERSION}.jar"
+VERSION="9.2.8.0"
+FULL_VERSION="${VERSION}"
+# FULL_VERSION="${VERSION}-20190809.220806-97" # Uncomment to use a snapshot
+JAR_FILE="jruby-complete-${FULL_VERSION}.jar"
+M2_CACHED_JAR="$HOME/.m2/repository/org/jruby/jruby-complete/${FULL_VERSION}/${JAR_FILE}"
 eval "DOWNLOAD_DIR=~/Downloads"
 DOWNLOADED_JAR="${DOWNLOAD_DIR}/${JAR_FILE}"
 
 cd libs
 rm -f bcpkix-jdk15on-*.jar bcprov-jdk15on-*.jar bctls-jdk15on-*.jar cparse-jruby.jar generator.jar jline-*.jar jopenssl.jar jruby-complete-*.jar parser.jar psych.jar readline.jar snakeyaml-*.jar
 
-if test -f "${M2_CACHED_JAR}"; then
+if [[ "${FULL_VERSION}" == "${VERSION}" ]] ; then
+  # Stable version
+  if [[ ! -f "${M2_CACHED_JAR}" ]] ; then
+    echo No "${M2_CACHED_JAR}" - Downloading.
+    mvn dependency:get -DremoteRepositories=http://repo1.maven.org/maven2/ \
+                   -DgroupId=org.jruby -DartifactId=jruby-complete -Dversion=${FULL_VERSION}
+  fi
   cp -a ${M2_CACHED_JAR} .
 else
+  # Snapshot version
   if test -f "${DOWNLOADED_JAR}"; then
     echo "Found downloaded JAR"
   else
-    echo No "${M2_CACHED_JAR}" or "${DOWNLOADED_JAR}" - Downloading.
-    wget "https://oss.sonatype.org/content/repositories/snapshots/org/jruby/jruby-complete/${BRANCH}-SNAPSHOT/${JAR_FILE}" -P "${DOWNLOAD_DIR}/"
+    echo No "${DOWNLOADED_JAR}" - Downloading.
+    wget "https://oss.sonatype.org/content/repositories/snapshots/org/jruby/jruby-complete/${VERSION}-SNAPSHOT/${JAR_FILE}" -P "${DOWNLOAD_DIR}/"
   fi
   cp ${DOWNLOADED_JAR} .
 fi
 
-unzip -j jruby-complete-${VERSION}.jar '*.jar'
-# rm jline-2.11.jar
+unzip -j ${JAR_FILE} '*.jar'
 
+# FIXME(uwe): Why do we delete these files?
 zip generator.jar -d json/ext/ByteListTranscoder.class
 zip generator.jar -d json/ext/OptionsReader.class
 zip generator.jar -d json/ext/Utils.class
@@ -37,12 +45,12 @@ find * -type f | grep "org/jruby/" | sed -e 's/\.java//g' | sort > ../../../over
 cd - >/dev/null
 
 while read p; do
-  unzip -Z1 libs/jruby-complete-${VERSION}.jar | grep "$p\\.class" > classes.txt
-  unzip -Z1 libs/jruby-complete-${VERSION}.jar | egrep "$p(\\\$[^$]+)*\\.class" >> classes.txt
+  unzip -Z1 libs/${JAR_FILE} | grep "$p\\.class" > classes.txt
+  unzip -Z1 libs/${JAR_FILE} | egrep "$p(\\\$[^$]+)*\\.class" >> classes.txt
   if [[ -s classes.txt ]] ; then
-    zip -d -@ libs/jruby-complete-${VERSION}.jar <classes.txt
+    zip -d -@ libs/${JAR_FILE} <classes.txt
     if [[ ! "$?" == "0" ]] ; then
-      zip -d libs/jruby-complete-${VERSION}.jar "$p\\.class"
+      zip -d libs/${JAR_FILE} "$p\\.class"
     fi
   fi
   rm classes.txt
