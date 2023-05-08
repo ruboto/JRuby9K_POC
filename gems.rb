04d834504ec0eef8843fabdca1f5a3d3c84f1e9f
Uwe Kubosch <uwe@datek.no>
2023-05-08 13:32:45 +0200

    require 'jruby' to access JRuby.runtime

--- gems.rb
+++ gems.rb
@@ -1,5 +1,5 @@
 source 'https://rubygems.org/'
 
 gem 'ruboto', '~>2.0.dev',
-    git: 'https://github.com/ruboto/ruboto.git'
+    git: 'https://github.com/ruboto/ruboto.git', branch: 'jruby_9_4'
     # path: '~/src/jruby/ruboto'
